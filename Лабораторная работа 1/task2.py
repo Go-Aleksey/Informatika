789cd3a3ae68015b59f16790aa0c4e532ea2a140
Go-Aleksey <golubev2.ad@edu.spbstu.ru>
2026-03-09 19:57:45 +0300

    task2.py

--- Лабораторная работа 1/task2.py
+++ Лабораторная работа 1/task2.py
@@ -1,11 +1,13 @@
-BYTES_ONE_CHAR = 4 # вес символа в байтах
+BYTES_ONE_CHAR = 4  # Вес одного символа в байтах
 
-disk_size = 1.44 # в мегабайтах
+disk_size = 1.44  # Размер дискеты в мегабайтах
 pages = 100
 strings = 50
 symbols = 25
 
-count_of_numbers = int(disk_size // (pages * strings * symbols * BYTES_ONE_CHAR / 1024**2)) # поделили объём дискеты на вес
-# всех символов книги в мегабайтах и привели к целочисленному формату
+book_size = pages * strings * symbols * BYTES_ONE_CHAR  # Вес книги в байтах
 
-print("Количество книг, помещающихся на дискету:", count_of_numbers)
+# Делим объём дискеты в мегабайтах на размер одной книги в мегабайтах
+count_of_books = int(disk_size // (book_size / 1024**2))
+
+print("Количество книг, помещающихся на дискету:", count_of_books)
