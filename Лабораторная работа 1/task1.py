f5f71b328f4ed3aa2fd8cbf87f6ae5bb262c88b7
Go-Aleksey <golubev2.ad@edu.spbstu.ru>
2026-03-09 19:56:40 +0300

    task1.py

--- Лабораторная работа 1/task1.py
+++ Лабораторная работа 1/task1.py
@@ -1,7 +1,14 @@
-numbers = [2, -93, -2, 8, None, -44, -1, -85, -14, 90, -22, -90, -100, -8, 38, -92, -45, 67, 53, 25]
+numbers = [
+    2, -93, -2, 8, None, -44, -1, -85, -14, 90,
+    -22, -90, -100, -8, 38, -92, -45, 67, 53, 25
+    ]
 
-index_missing_item = 4 # нашли пропущенный элемент
+index_missing_item = 4  # Нашли номер пропущенного элемента
 
-numbers[index_missing_item] = (sum(numbers[:index_missing_item] + numbers[index_missing_item+1:])) / len(numbers) # Заменили None на среднее арифметическое
+sum_of_numbers = sum(numbers[:index_missing_item] + numbers[index_missing_item+1:])
+count_of_numbers = len(numbers)  # Нашли количество элементов всего списка
+
+# Заменяем None на среднее арифметическое
+numbers[index_missing_item]=sum_of_numbers / count_of_numbers
 
 print("Измененный список:", numbers)
