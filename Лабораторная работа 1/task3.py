4e2d70e19dc4d8adcd82cf7c491de428e8fca225
Go-Aleksey <golubev2.ad@edu.spbstu.ru>
2026-03-09 19:58:42 +0300

    task3.py

--- Лабораторная работа 1/task3.py
+++ Лабораторная работа 1/task3.py
@@ -1,8 +1,9 @@
 list_players = ["Маша", "Петя", "Саша", "Оля", "Кирилл", "Коля"]
-middle_index = len(list_players) // 2 # нашли индекс середины
 
-first_team = list_players[:middle_index] # первая команда - от начала до середины, т е взяли слайс полсписка
-second_team = list_players[middle_index:] # вторая команда - с середины до конца, т е взяли слайс вторых полсписка
+middle_index = len(list_players) // 2  # Нашли индекс середины
+
+first_team = list_players[:middle_index]  # Взяли слайс первые полсписка
+second_team = list_players[middle_index:]  # Взяли слайс вторые полсписка
 
 print(first_team)
 print(second_team)
